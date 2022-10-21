536b7d53ce21891338866f49798bcdef3478a9c1
Lucaponz91 <lucapo1991@msn.com>
2022-10-21 15:35:22 +0200

    primo commit

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['film','serie TV','food','moda','digitale','sport'];
+        foreach($categories as $name){
+            $c = new Category();
+            $c->name = $name;
+            $c->slug = Str::slug($name);
+
+            $c->save();
+        }
+    }
+}
