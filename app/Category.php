9cb473542e0e3b714695923903ec8ba25b168d3d
Lucaponz91 <lucapo1991@msn.com>
2022-10-23 19:40:30 +0200

    fix

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+        // select * from `posts` where posts.category_id === ?
+        // return $this->hasMany('App\Post','category_id','id');
+    }
 }
